24a96e2a50bf74a42bc72a2e87d708a2b40a4a66
hrtarposhyan <hrtarposhyan@gmail.com>
2020-03-06 08:04:04 -0800

    Create Data Models

---
+++ LIbraryManagmentRepository/LIbraryManagmentRepository/Data/Model/Author.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LIbraryManagmentRepository.Data.Model
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+}
---
+++ LIbraryManagmentRepository/LIbraryManagmentRepository/Data/Model/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LIbraryManagmentRepository.Data.Model
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+    }
+}
