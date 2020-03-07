90588e117b04ba1198e278d2624afe5c08f95dc6
hrtarposhyan <hrtarposhyan@gmail.com>
2020-03-07 02:05:38 -0800

    Implementing The Repositories

---
+++ LIbraryManagmentRepository/LIbraryManagmentRepository/Data/Repository/CustomerRepository.cs
@@ -0,0 +1,16 @@
+﻿using LIbraryManagmentRepository.Data.Interfaces;
+using LIbraryManagmentRepository.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LIbraryManagmentRepository.Data.Repository
+{
+    public class CustomerRepository: Repository<Customer>,ICustomerRepository
+    {
+        public CustomerRepository(LibraryDbContext context) : base(context)
+        {
+
+        }
+    }
+}
